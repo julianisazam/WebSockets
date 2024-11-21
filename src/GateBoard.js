9147ade371cf87a286b89c82c4c68d5ec733c103
Emanuel M <emanuel.munera1@gmail.com>
2024-11-21 18:23:33 -0500

    Organizado el front para consumir el websocket de flights

--- src/GateBoard.js
+++ src/GateBoard.js
@@ -1,71 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import SockJS from 'sockjs-client'; 
-import { Stomp } from '@stomp/stompjs'; 
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper }
-from '@mui/material'; 
-import GateForm from './GateForm'; 
-
+import React, { useState, useEffect } from "react";
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import GateForm from "./GateForm";
 
 const GateBoard = () => {
+  const [flightData, setFlightData] = useState([]);
+  useEffect(() => {
+    const socket = new SockJS("http://localhost:8080/air-websocket");
+    const stompClient = Stomp.over(socket);
 
-const [gateData, setGateData] = useState([]);
-useEffect(() => {
+    stompClient.connect({}, () => {
+      stompClient.subscribe("/topic/flights", (message) => {
+        if (message.body) {
+          const updatedFlight = JSON.parse(message.body);
+          setFlightData((prevData) => {
+            const updatedData = prevData.filter(
+              (data) => data.flight !== updatedFlight.flight
+            );
+            return [...updatedData, updatedFlight];
+          });
+        }
+      });
+    });
 
-const socket = new SockJS('http://localhost:8080/air-websocket');
-const stompClient = Stomp.over(socket); 
-
-stompClient.connect({}, () => {
-
-stompClient.subscribe('/topic/gates', (message) => {
-if (message.body) {
-const updatedGate = JSON.parse(message.body); 
-setGateData(prevData => {
-
-const updatedData = prevData.filter(data => data.gate !== updatedGate.gate);
-return [...updatedData, updatedGate]; 
-});
-}
-});
-});
-
-return () => {
-stompClient.disconnect();
-};
-}, []); 
-return (
-<div>
-{/* Renderizar el formulario para gestionar datos de puertas */}
-<GateForm />
-{/* Contenedor de la tabla con los datos de las puertas */}
-<TableContainer component={Paper} sx={{ mt: 3 }}>
-<Table>
-<TableHead>
-<TableRow>
-{/* Encabezados de la tabla */}
-<TableCell>Latitude</TableCell>
-<TableCell>Flight Number</TableCell>
-<TableCell>Heading</TableCell>
-<TableCell>AirSpeed</TableCell>
-<TableCell>altitude</TableCell>
-</TableRow>
-</TableHead>
-<TableBody>
-{/* Renderizar filas dinámicas basadas en los datos del estado */}
-{gateData.map((gateInfo, index) => (
-<TableRow key={index}>
-{/* Celdas con los datos de cada puerta */}
-<TableCell>{gateInfo.latitude}</TableCell>
-
-<TableCell>{gateInfo.flightNumber}</TableCell>
-<TableCell>{gateInfo.heading}</TableCell>
-<TableCell>{gateInfo.airSpeed}</TableCell>
-<TableCell>{gateInfo.altitude}</TableCell>
-</TableRow>
-))}
-</TableBody>
-</Table>
-</TableContainer>
-</div>
-);
+    return () => {
+      stompClient.disconnect();
+    };
+  }, []);
+  return (
+    <div>
+      {/* Renderizar el formulario para gestionar datos de puertas */}
+      <GateForm />
+      {/* Contenedor de la tabla con los datos de las puertas */}
+      <TableContainer component={Paper} sx={{ mt: 3 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {/* Encabezados de la tabla */}
+              <TableCell>Latitude</TableCell>
+              <TableCell>Longitude</TableCell>
+              <TableCell>Flight Code</TableCell>
+              <TableCell>Heading</TableCell>
+              <TableCell>Speed</TableCell>
+              <TableCell>Altitude</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* Renderizar filas dinámicas basadas en los datos del estado */}
+            {flightData.map((flightInfo, index) => (
+              <TableRow key={index}>
+                {/* Celdas con los datos de cada puerta */}
+                <TableCell>{flightInfo.latitude}</TableCell>
+                <TableCell>{flightInfo.longitude}</TableCell>
+                <TableCell>{flightInfo.flightCode}</TableCell>
+                <TableCell>{flightInfo.heading}</TableCell>
+                <TableCell>{flightInfo.speed}</TableCell>
+                <TableCell>{flightInfo.altitude}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 export default GateBoard;
